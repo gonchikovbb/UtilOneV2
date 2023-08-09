cd9a2dbe422530e3195db225e5f9cc3e02bafede
gonchikovbb <gonchikovbb@gmail.com>
2023-08-09 16:19:08 +0800

    fix:kiss code

--- app/public/index.php
+++ app/public/index.php
@@ -2,79 +2,48 @@
 
 $urlGet = 'https://jsonplaceholder.typicode.com/comments';
 
-$context = stream_context_create(array(
-    'http' => array(
-        'method' => 'GET',
-    ),
-));
+$ch = curl_init($urlGet);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-$result = file_get_contents($urlGet, false, $context);
+$result = curl_exec($ch);
+curl_close($ch);
 
 $posts = json_decode($result, true);
 
 // общее количество постов с комментариями
+$postsId = array_column($posts, 'postId');
 
-for ($i = 0; $i < count($posts); $i++ ) {
+$postsUnique = array_unique($postsId);
 
-    if (!isset($posts[$i+1])) {
-        $countPost[] = $posts[$i]['postId'];
-        break;
-    } elseif ($posts[$i]['postId'] === $posts[$i+1]['postId']) {
-        continue;
-    } else {
-        $countPost[] = $posts[$i]['postId'];
-    }
-}
-
-$postsCount = count($countPost);
-//print_r($postsCount);
-//echo PHP_EOL;
+$postsCount = count($postsUnique);
 
 // id поста с максимумом комментов
-
-$maxCommentPostId = $posts[0]['postId'];
-
-$maxCount = 0;
-$curCount = 0;
-
-for ($i = 0; $i < count($posts); $i++ ) {
-
-    if (!isset($posts[$i+1])) {
-        break;
-    } elseif ($posts[$i]['postId'] === $posts[$i+1]['postId']) {
-        $curCount++;
-    } elseif($curCount > $maxCount) {
-        $maxCommentPostId = $posts[$i]['postId'];
-        $curCount = 0;
-    }
-}
-//print_r($maxCommentPostId);
-//echo PHP_EOL;
+$maxCommentPostId = array_key_first(array_count_values($postsId));
 
 // максимальное количество комментов
-
 foreach ($posts as $post) {
     if ($post['postId'] === $maxCommentPostId) {
         $maxCommentCount = $post['id'];
         break;
     }
 }
-//print_r($maxCommentCount);
 
-$result = json_encode([
+// данные POST-запроса
+$data = json_encode([
     'postsCount' => $postsCount,
     'maxCommentPostId' => $maxCommentPostId,
     'maxCommentCount' => $maxCommentCount,
 ]);
 
-//print_r($result);
-
+// url, на который отправляет данные
 $urlPost = 'https://webhook.site/33dfbbe4-b16b-4a82-acf7-33667697b21f';
 
-$params = array($result);
-$result = file_get_contents($urlPost, false, stream_context_create(array(
-    'http' => array(
-        'method'  => 'POST',
-        'content' => http_build_query($params)
-    )
-)));
+$ch = curl_init($urlPost);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+var_dump($response);
